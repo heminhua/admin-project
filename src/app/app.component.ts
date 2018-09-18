391a3a483f36d201c225542f5262f695b7290a4b
heminhua <heminhua2013@163.com>
2018-09-18 16:54:44 +0800

    create project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import * as $ from 'jquery'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'admin-project';
+  constructor(
+   
+  ) {
+  }
+  ngOnInit(){
+
+  }
 }
+
+
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+// import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AppHeaderComponent } from './components/common/app-header/app-header.component';
+import { AppNavComponent } from './components/common/app-nav/app-nav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AppHeaderComponent,
+    AppNavComponent,
   ],
   imports: [
     BrowserModule,
